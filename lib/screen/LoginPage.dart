c31d33701ca8a5d8b7c126fad9da230b586ef931
Mansi joshi <joshimansi32@gmail.com>
2023-03-04 12:24:43 +0530

    Update LoginPage.dart

--- lib/screen/LoginPage.dart
+++ lib/screen/LoginPage.dart
@@ -1,6 +1,9 @@
+import 'package:child_vaccination/shared/validity.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,24 +13,197 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  // setiting global values
-  final _formkey = GlobalKey<FormState>();
+  final formkey = GlobalKey<FormState>();
   final _emailTextController = TextEditingController();
-  final _paswordTextController = TextEditingController();
-  final _focusEmail = FocusNode();
-  final _focusPassword = FocusNode();
-  bool _isProcessing = false;
+  final _passwordTextController = TextEditingController();
+  String email = "";
+  String password = "";
+  bool _isLoading = false;
   bool passwordVisible = true;
-  var error;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: const [
-          Text('Hello'),
-        ],
-      ),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(
+              color: Theme.of(context).primaryColor,
+            ))
+          : SingleChildScrollView(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+                child: Form(
+                  key: formkey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      const Text(
+                        "Immunify",
+                        style: TextStyle(
+                            fontSize: 40, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'Unlock exclusive resources and tools for your child\'s success - Login now!',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      // Image to be added
+                      // Email
+                      TextFormField(
+                        controller: _emailTextController,
+                        validator: (value) => Validator.validateEmail(
+                            email: _emailTextController.text),
+                        decoration: InputDecoration(
+                          hintText: "Email",
+                          prefixIcon: Icon(
+                            Icons.email_outlined,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Theme.of(context).primaryColor,
+                              width: 1.0,
+                            ),
+                          ),
+                          errorBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(6),
+                            borderSide: const BorderSide(
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                        onChanged: (val) {
+                          setState(() {
+                            email = val;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 15),
+                      // Password
+                      TextFormField(
+                        controller: _passwordTextController,
+                        validator: (value) => Validator.validatePassword(
+                            password: _passwordTextController.text),
+                        obscureText: passwordVisible,
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                          prefixIcon: Icon(
+                            Icons.password_outlined,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          suffixIcon: IconButton(
+                            icon: Icon(passwordVisible
+                                ? Icons.visibility_off_outlined
+                                : Icons.visibility_outlined),
+                            onPressed: () {
+                              setState(() {
+                                passwordVisible = !passwordVisible;
+                              });
+                            },
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Theme.of(context).primaryColor,
+                              width: 1.0,
+                            ),
+                          ),
+                          errorBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(6),
+                            borderSide: const BorderSide(
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                        onChanged: (val) {
+                          setState(() {
+                            password = val;
+                          });
+                        },
+                      ),
+                      // forget password option
+                      const SizedBox(height: 8),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          "Forget Password ?",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 160, 195, 224),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 13),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Theme.of(context).primaryColor,
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            "Sign In",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                      // Google Sign In option
+                      const SizedBox(height: 13),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Theme.of(context).primaryColor,
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            "Google Sign In",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                      // Register the account
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text.rich(
+                        TextSpan(
+                            text: "Don't have an account?",
+                            style: const TextStyle(
+                                color: Colors.black, fontSize: 14),
+                            children: <TextSpan>[
+                              TextSpan(
+                                  text: "Register here",
+                                  style: const TextStyle(
+                                      color: Colors.black,
+                                      decoration: TextDecoration.underline),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () {})
+                            ]),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
     );
   }
 }
