a86d23228c017e533ab0ea035c2508c3399b85b7
Mansi joshi <joshimansi32@gmail.com>
2023-04-03 21:29:28 +0530

    Update Profile.dart

--- lib/screen/pages/Profile.dart
+++ lib/screen/pages/Profile.dart
@@ -1,5 +1,7 @@
 import 'package:child_vaccination/helper/helperFunction.dart';
-import 'package:child_vaccination/screen/childCreate.dart';
+import 'package:child_vaccination/screen/LoginPage.dart';
+import 'package:child_vaccination/screen/ResetPassword.dart';
+import 'package:child_vaccination/screen/setting.dart';
 import 'package:child_vaccination/services/authenticationService.dart';
 import 'package:flutter/material.dart';
 
@@ -11,40 +13,44 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
-  String Pname = "";
+  String name = "";
+  String email = "";
   bool _isLoading = false;
+  bool _isVerified = false;
   AuthenticationService authenticationService = AuthenticationService();
 
-  @override
   @override
   void initState() {
     super.initState();
     gettingUserData();
   }
 
-  // function to get the user data
   Future<void> gettingUserData() async {
+    // show the loading detector
     setState(() {
       _isLoading = true;
     });
 
     try {
-      // fetch the username from shared preferences
-      String? userName = await HelperFunction.getUserName();
-      if (userName == null) {
-        // user not found
+      // fetch the user Email from shared preference
+      await HelperFunction.getUserEmail().then((value) {
         setState(() {
-          Pname = "Unknown";
+          email = value!;
         });
-      } else {
+      });
+      // fetch the name from shared prefernce
+      await HelperFunction.getUserName().then((value) {
         setState(() {
-          Pname = userName;
+          name = value!;
         });
-      }
+      });
     } catch (e) {
-      throw (e);
+      setState(() {
+        name = "Unknown";
+        email = "unknown@Email";
+      });
     } finally {
-      // hide loading indicator
+      // hide the loading indicator
       setState(() {
         _isLoading = false;
       });
@@ -54,68 +60,207 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
-        toolbarHeight: 100,
       ),
-      drawer: Drawer(),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              // image
-              const CircleAvatar(
-                radius: 60,
-                backgroundImage: AssetImage('assets/images/profile.png'),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 50),
+          children: <Widget>[
+            ListTile(
+              onTap: () async {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const SettingPage()),
+                );
+              },
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              leading: const Icon(
+                Icons.settings_applications_rounded,
+                color: Colors.blueGrey,
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              // name
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Theme.of(context).primaryColor,
+              title: const Text(
+                "Settings",
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      '$Pname',
-                      style: TextStyle(
-                        fontSize: MediaQuery.of(context).size.width * 0.05,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => CreateNewChild(
-                parentName: Pname,
               ),
             ),
-          ).then((value) {
-            if (value != null && value is String) {
-              // handle the result if needed
-            }
-          });
-        },
-        backgroundColor: Colors.black,
-        child: Icon(Icons.add),
+            ListTile(
+              onTap: () async {
+                showDialog(
+                    barrierDismissible: false,
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text("Reset Password"),
+                        content:
+                            const Text("Are you sure to reset the password?"),
+                        actions: [
+                          IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: const Icon(
+                              Icons.cancel,
+                              color: Colors.red,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () async {
+                              await authenticationService.SignOut();
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const ResetPassword()),
+                              );
+                            },
+                            icon: const Icon(
+                              Icons.done,
+                              color: Colors.green,
+                            ),
+                          ),
+                        ],
+                      );
+                    });
+              },
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              leading: const Icon(
+                Icons.password_rounded,
+                color: Colors.blueGrey,
+              ),
+              title: const Text(
+                "Reset Password",
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListTile(
+              onTap: () async {
+                showDialog(
+                    barrierDismissible: false,
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text("LogOut"),
+                        content: const Text("Are you sure to logout?"),
+                        actions: [
+                          IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: const Icon(
+                              Icons.cancel,
+                              color: Colors.red,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () async {
+                              await authenticationService.SignOut();
+                              Navigator.of(context).pushAndRemoveUntil(
+                                  MaterialPageRoute(
+                                      builder: (context) => const LoginPage()),
+                                  (route) => false);
+                            },
+                            icon: const Icon(
+                              Icons.done,
+                              color: Colors.green,
+                            ),
+                          ),
+                        ],
+                      );
+                    });
+              },
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              leading: const Icon(
+                Icons.exit_to_app_outlined,
+                color: Colors.blueGrey,
+              ),
+              title: const Text(
+                "Log out",
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 100),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const CircleAvatar(
+              radius: 60,
+              backgroundImage: AssetImage('assets/images/profile.png'),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "User Name",
+                        style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "$name",
+                        style: const TextStyle(
+                            fontSize: 17,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  const Divider(height: 20, color: Colors.white),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "Email",
+                        style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "$email",
+                        style: const TextStyle(
+                            fontSize: 17,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
