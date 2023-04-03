1a1860efd1ef6b8ae7f625bcaeb1ef64b454ffc8
Mansi joshi <joshimansi32@gmail.com>
2023-04-03 21:29:54 +0530

    Create chat.dart

---
+++ lib/screen/pages/chat.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        automaticallyImplyLeading: false,
+      ),
+    );
+  }
+}
